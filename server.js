e73bf1e93d0a74d39aec99eab07ac684e717ffc0
Aaron <aaronmcavinue@gmail.com>
2016-11-07 23:08:28 +0000

    Implement delete functionality.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
  * Modules
  */
 var express = require('express');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
 
 /**
  * Variables
@@ -12,7 +14,12 @@ var Storage = {
     this.items.push(item);
     this.setId += 1;
     return item;
-  } 
+  },
+  remove: function(index) {
+    if (index) {
+      delete this.items[index];
+    }
+  }
 };
 
 var createStorage = function() {
@@ -33,12 +40,44 @@ var app = express();
 /**
  * Routes
  */
-app.use(express.static('public'));  // Set the root (/) route to the public directory.
+// Set the root (/) route to the public directory.
+app.use(express.static('public'));
 
 // GET /items
 app.get('/items', function(request, response) {
     response.json(storage.items);
 });
 
+// POST /items
+app.post('/items', jsonParser, function(request, response) {
+  if(!('name' in request.body)) {
+    return response.sendStatus(400);
+  }
+  
+  var item = storage.add(request.body.name);
+  response.status(201).json(item);
+});
+
+//DELETE /items/<id>
+app.delete('/items/:id', function(request, response) {
+  var foundItem = false;
+  var arrayIndex;
+  
+  for (var i = 0; i < storage.items.length; i++) {
+    if (Number(request.params.id) === storage.items[i].id) {
+      foundItem = true;
+      arrayIndex = i;
+    }
+  }
+  
+  if (!foundItem) {
+    return response.sendStatus(404);
+  } else {
+    storage.remove(i);
+    return response.sendStatus(200);
+  }
+  
+});
+
 // Run the local server.
-app.listen(process.env.PORT || 8080, process.env.IP);
+app.listen(process.env.PORT || process.env.IP);
